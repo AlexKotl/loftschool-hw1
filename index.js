44c61ee70e17900a864e0eba7dac8077b4f880a1
Alex <slicer256@gmail.com>
2019-06-09 13:52:15 +0300

    processing files by letter folders

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Running")
+const fs = require('fs');
+const path = require('path');
+
+var config = {
+    sourceDir: 'sample',
+    destDir: 'ordered',
+}
+
+function processDir(dir) {
+    fs.readdirSync(path.join(__dirname, dir), {withFileTypes: true}).forEach(function(file) {
+        if (file.isDirectory()) {
+            processDir(path.join(dir, file.name));
+        }
+        else {
+            let letter = file.name[0].toUpperCase();
+            if (!letter.match(/([A-Z])/)) {
+                letter = 'other';
+            }
+            
+            // create letter dir
+            if (!fs.existsSync(path.join(__dirname, config.destDir, letter))) {
+                fs.mkdirSync(path.join(__dirname, config.destDir, letter));
+            }
+            
+            fs.copyFileSync(path.join(__dirname, dir, file.name), path.join(__dirname, config.destDir, letter, file.name));
+        }
+    });
+}
+
+if (!fs.existsSync(config.destDir)) {
+    fs.mkdirSync(config.destDir);
+}
+
+processDir(config.sourceDir);
+
+
