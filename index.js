74405bf056a86e7849a7970a7da59a98d0bd9159
Alex <slicer256@gmail.com>
2019-06-09 14:24:19 +0300

    eslint support

--- index.js
+++ index.js
@@ -2,47 +2,46 @@ const fs = require('fs');
 const path = require('path');
 
 var config = {
-    sourceDir: process.argv[2] || 'sample',
-    destDir: process.argv[3] || 'ordered',
-    deleteSource: process.argv.indexOf('-d') !== -1,
-}
+  sourceDir: process.argv[2] || 'sample',
+  destDir: process.argv[3] || 'ordered',
+  deleteSource: process.argv.indexOf('-d') !== -1
+};
 
-function processDir(dir) {
-    fs.readdirSync(path.join(__dirname, dir), {withFileTypes: true}).forEach(function(file) {
-        if (file.isDirectory()) {
-            processDir(path.join(dir, file.name));
-        }
-        else {
-            let letter = file.name[0].toUpperCase();
-            if (!letter.match(/([A-Z])/)) {
-                letter = 'other';
-            }
-            
-            // create letter dir
-            if (!fs.existsSync(path.join(__dirname, config.destDir, letter))) {
-                fs.mkdirSync(path.join(__dirname, config.destDir, letter));
-            }
-            
-            fs.copyFileSync(path.join(__dirname, dir, file.name), path.join(__dirname, config.destDir, letter, file.name));
-            
-            if (config.deleteSource) {
-                fs.unlinkSync(path.join(__dirname, dir, file.name));
-            }
-        }
-    });
-    
-    if (config.deleteSource) {
-        fs.rmdirSync(path.join(__dirname, dir));
+function processDir (dir) {
+  fs.readdirSync(path.join(__dirname, dir), { withFileTypes: true }).forEach(function (file) {
+    if (file.isDirectory()) {
+      processDir(path.join(dir, file.name));
+    } else {
+      let letter = file.name[0].toUpperCase();
+      if (!letter.match(/([A-Z])/)) {
+        letter = 'other';
+      }
+
+      // create letter dir
+      if (!fs.existsSync(path.join(__dirname, config.destDir, letter))) {
+        fs.mkdirSync(path.join(__dirname, config.destDir, letter));
+      }
+
+      fs.copyFileSync(path.join(__dirname, dir, file.name), path.join(__dirname, config.destDir, letter, file.name));
+
+      if (config.deleteSource) {
+        fs.unlinkSync(path.join(__dirname, dir, file.name));
+      }
     }
+  });
+
+  if (config.deleteSource) {
+    fs.rmdirSync(path.join(__dirname, dir));
+  }
 }
 
 if (!fs.existsSync(config.destDir)) {
-    fs.mkdirSync(config.destDir);
+  fs.mkdirSync(config.destDir);
 }
 
 if (!fs.existsSync(config.sourceDir)) {
-    console.log('No such source folder: ', config.sourceDir);
-    return;
+  console.log('No such source folder: ', config.sourceDir);
+  process.exit();
 }
 
 console.log('Ordering files from ', config.sourceDir, 'into', config.destDir, '. Will remove source folder:', config.deleteSource);
@@ -50,7 +49,3 @@ console.log('Ordering files from ', config.sourceDir, 'into', config.destDir, '.
 processDir(config.sourceDir);
 
 console.log('Processing files done.');
-
-
-
-
