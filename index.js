5bb390a302bb1afc18410ea875f3896685f9369c
Alex <slicer256@gmail.com>
2019-06-12 01:17:22 +0300

    refactor script using async/await

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ exists = util.promisify(fs.exists);
 mkdir = util.promisify(fs.mkdir);
 unlink = util.promisify(fs.unlink);
 
-async function processDir (dir) {
+processDir = async (dir) => {
   const dirContent = await readdir(path.join(__dirname, dir), { withFileTypes: true });
   dirContent.forEach(async function (file) {
     if (file.isDirectory()) {
-      processDir(path.join(dir, file.name));
+      await processDir(path.join(dir, file.name));
     } else {
       let letter = file.name[0].toUpperCase();
       if (!letter.match(/([A-Z])/)) {
@@ -26,35 +26,47 @@ async function processDir (dir) {
       }
 
       // create letter dir
-      if (!fs.existsSync(path.join(__dirname, config.destDir, letter))) {
-        fs.mkdirSync(path.join(__dirname, config.destDir, letter));
+      const folderExists = await exists(path.join(__dirname, config.destDir, letter));
+      if (!folderExists) {
+        try {
+          await mkdir(path.join(__dirname, config.destDir, letter));
+        }
+        catch (error) {
+          // skip error
+        }
+        
       }
 
       console.log(' - copying file', file.name);
-      copyFile(path.join(__dirname, dir, file.name), path.join(__dirname, config.destDir, letter, file.name));
+      await copyFile(path.join(__dirname, dir, file.name), path.join(__dirname, config.destDir, letter, file.name));
 
       if (config.deleteSource) {
-        fs.unlinkSync(path.join(__dirname, dir, file.name));
+        await unlink(path.join(__dirname, dir, file.name));
       }
     }
   });
 
   if (config.deleteSource) {
-    fs.rmdirSync(path.join(__dirname, dir));
+    await rmdir(path.join(__dirname, dir));
   }
-}
-
-if (!fs.existsSync(config.destDir)) {
-  fs.mkdirSync(config.destDir);
-}
-
-if (!fs.existsSync(config.sourceDir)) {
-  console.log('No such source folder: ', config.sourceDir);
-  process.exit();
 }
 
 console.log('Ordering files from ', config.sourceDir, 'into', config.destDir, '. Will remove source folder:', config.deleteSource);
 
-processDir(config.sourceDir);
+const app = async () => {
+  const isExists = await exists(config.destDir);
+  if (!isExists) {
+    await mkdir(config.destDir);
+  }
+  
+  const sourceFolderExists = await exists(config.sourceDir);
+  if (!sourceFolderExists) {
+    console.log('No such source folder: ', config.sourceDir);
+    process.exit(1);
+  } 
+
+  await processDir(config.sourceDir);
+};
+app();
 
 console.log('Processing files done.');
